7cdefc94e91fa14a876b26e63c0b394c92b8a43e
Kyle Richard <Kyle.Richard@altec.com>
2018-06-25 10:20:29 -0500

    - Android NUnit Test

--- TicTacToe.UITests/AppInitializer.cs
+++ TicTacToe.UITests/AppInitializer.cs
@@ -27,7 +27,8 @@ namespace TicTacToe.UITests
                     .Android
                     // TODO: Update this path to point to your Android app and uncomment the
                     // code if the app is not included in the solution.
-                    .ApkFile ("../../../Droid/bin/Debug/TicTacToe.apk")
+                    .ApkFile ("../../../Droid/bin/Debug/com.CrimsonKyle.TicTacToe.apk")
+                    .EnableLocalScreenshots()
                     .StartApp();
             }
 
